d4fc9383b97ab8ef687399673493737d0422e358
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2024-02-13 11:56:59 +0530

    Update Qn-04.cpp

--- Qn-04.cpp
+++ Qn-04.cpp
@@ -1,19 +1,22 @@
-//WAP in C to calculate LCM of two numbers.
+//Write  a function  in C to calculate LCM of two numbers.
 
 #include<stdio.h>
+int lcm (int , int ) ;
 int main ()
 {
     int a ,b ;
     printf("Enter two number : " );
     scanf("%d%d" , &a , &b) ;
     
+    printf("LCM is %d" , lcm(a,b)) ;
+    return 0 ;
+}
+int lcm (int a, int b)
+{
     int i ;
     for (i=(a>b?a:b) ; i<=a*b ; i=i+(a>b?a:b))
         if (i%a==0 && i%b==0)
-        {
-            printf("LCM is %d" , i) ;
             break ;
-        }
-
-    return 0 ;
+        
+    return i ;
 }
